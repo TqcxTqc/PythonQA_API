ce442990279b60666565fbbbba1fb3db6b95e7f4
Vitali Dambajev <vitali.dambajev@insly.com>
2021-02-05 22:06:15 +0200

    Removed print in test_module function and add additional line after assert

--- TestFunction/test_metod_url/test_function.py
+++ TestFunction/test_metod_url/test_function.py
@@ -1,9 +1,7 @@
-import pytest
 import requests
 
 
-def test_module(base_url,base_status_code):
+def test_module(base_url, base_status_code):
     response = requests.get(base_url)
-    print(response.status_code)
 
     assert response.status_code == base_status_code
